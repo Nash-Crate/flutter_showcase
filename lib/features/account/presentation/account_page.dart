264f4b568b4a07db04f69b94fc9ed1dc7986333d
Prageeth Monasha <spmonasha@gmail.com>
2026-04-26 19:28:10 +0200

    [f/account] account feature initiated;

---
+++ lib/features/account/presentation/account_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+/// Account page
+class AccountPage extends StatelessWidget {
+  /// constructor
+  const AccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
